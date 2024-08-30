5fbe7ea1f23a5e0726e4073dc7eca4933344bb7d
Meet Thakur <99238677+MeetThakur@users.noreply.github.com>
2024-08-30 21:47:25 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function Home() {
-  const [location, setLocation] = useState('');
-  const [riskLevel, setRiskLevel] = useState('Low');
+  const [location, setLocation] = useState("");
+  const [riskLevel, setRiskLevel] = useState("Low");
   const [volunteers, setVolunteers] = useState(10);
   const [reports, setReports] = useState(1);
 
@@ -20,7 +20,7 @@ function Home() {
     } else {
       score += 1;
     }
-  
+
     if (volunteers >= 5) {
       score -= 1;
     } else if (volunteers >= 3) {
@@ -30,7 +30,7 @@ function Home() {
     } else {
       score += 5;
     }
-  
+
     if (reports >= 10) {
       score += 5;
     } else if (reports >= 5) {
@@ -43,39 +43,37 @@ function Home() {
       score += 1;
     }
 
-
     if (score >= 13) {
-      return 'Very High';
+      return "Very High";
     } else if (score >= 10) {
-      return 'High';
+      return "High";
     } else if (score >= 7) {
-      return 'Moderate';
+      return "Moderate";
     } else if (score >= 4) {
-      return 'Low';
+      return "Low";
     } else {
-      return 'Very Low';
+      return "Very Low";
     }
   }
 
-
   function RiskColor() {
-    if (riskLevel === 'Very High') {
-      return 'bg-red-500';
-    } else if (riskLevel === 'High') {
-      return 'bg-red-400';
-    } else if (riskLevel === 'Moderate') {
-      return 'bg-yellow-400';
-    } else if (riskLevel === 'Low') {
-      return 'bg-green-400';
+    if (riskLevel === "Very High") {
+      return "bg-red-500";
+    } else if (riskLevel === "High") {
+      return "bg-red-400";
+    } else if (riskLevel === "Moderate") {
+      return "bg-yellow-400";
+    } else if (riskLevel === "Low") {
+      return "bg-green-400";
     } else {
-      return 'bg-green-500';
+      return "bg-green-500";
     }
   }
   useEffect(() => {
     RiskColor();
     setRiskLevel(calculateRiskScore());
     getLocation();
-  }, [volunteers,reports,riskLevel]);
+  }, [volunteers, reports, riskLevel]);
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -85,39 +83,49 @@ function Home() {
           setLocation(`Latitude: ${latitude}, Longitude: ${longitude}`);
         },
         (error) => {
-          console.error('Error getting location:', error);
-        }
+          console.error("Error getting location:", error);
+        },
       );
     } else {
-      console.error('Geolocation is not supported by this browser.');
+      console.error("Geolocation is not supported by this browser.");
     }
   }
 
   function sos() {
-    alert('SOS sent');
+    alert("SOS sent");
   }
 
   return (
     <>
-    <div className='flex justify-center'>
-      <h1 className='text-4xl font-extrabold text-center mt-4 text-white'>
-        Suraksha</h1>
-    </div>
-    <div className={`w-full flex place-content-center mt-20 p-4 font-bold text-2xl ${RiskColor()}`}>
-        Risk Level : {riskLevel}
-    </div>
+      <div className="flex justify-center">
+        <h1 className="text-[5vh] font-extrabold text-center mt-4 text-white">
+          Suraksha
+        </h1>
+      </div>
 
-    <div className='text-lg font-extrabold text-center mt-32 text-white'>
-        Your Location: <br />{location} <br/> Volunteers Nearby : {volunteers}
-        <br /> Reports by User : {reports}
-    </div>
-    <div className='flex justify-center items-center  w-full mt-36'>
-        <button className='text-center p-2 bg-red-500 rounded-xl font-extrabold text-white text-5xl' onClick={sos}>
-          SEND SOS
-        </button>
-    </div>
+      <div className="flex flex-col gap-[15vh] mt-[10vh]">
+        <div
+          className={`w-full flex place-content-center p-[2vh] font-bold text-[3vh] ${RiskColor()}`}
+        >
+          Risk Level : {riskLevel}
+        </div>
+
+        <div className="text-[2vh] font-extrabold text-center text-white">
+          Your Location: <br />
+          {location} <br /> Volunteers Nearby : {volunteers}
+          <br /> Reports by User : {reports}
+        </div>
+        <div className="flex justify-center items-center  w-full">
+          <button
+            className="text-center p-[1vh] bg-red-500 rounded-xl font-extrabold text-white text-[5vh]"
+            onClick={sos}
+          >
+            SEND SOS
+          </button>
+        </div>
+      </div>
     </>
   );
 }
-  
+
 export default Home;
